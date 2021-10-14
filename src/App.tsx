c00559c144654259167a77917d7f265a0304331b
Samer Kayali <samerkkr@gmail.com>
2021-10-13 22:17:29 -0300

    feat: added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Packages
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// Pages
+import { DashBoard } from "pages/Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={DashBoard} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
