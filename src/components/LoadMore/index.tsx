9085258d620f529d23fca2e0bb8eb703a66a002a
Samer Kayali <samerkkr@gmail.com>
2021-10-15 11:41:03 -0300

    feat: animation loading more component

--- src/components/LoadMore/index.tsx
+++ src/components/LoadMore/index.tsx
@@ -1,18 +1,22 @@
 import { AiOutlineReload } from "react-icons/ai";
+import "./style.css";
 
-interface Props{
-    onClick:()=>void
+interface Props {
+  onClick: () => void;
+  isLoading?: boolean;
 }
 
-export default function LoadMore({onClick}:Props) {
+export default function LoadMore({ onClick, isLoading }: Props) {
   return (
     <div
       className="d-flex align-items-center"
       onClick={onClick}
       style={{ cursor: "pointer" }}
     >
-      <AiOutlineReload size="30px" />
-      <h3 className="mb-0">Loading More...</h3>
+      <AiOutlineReload size="30px" className={!isLoading ? "" : "loading"} />
+      <h3 className={`mb-0 ${!isLoading ? "" : "loading-dots"}`}>
+        Loading More
+      </h3>
     </div>
   );
 }
