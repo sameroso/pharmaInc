c8330e995e810a24ab4f53a3cdd94e65e8b92066
Samer Kayali <samerkkr@gmail.com>
2021-10-16 15:08:01 -0300

    feat: added css vriables and colors

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "assets/scss/index.scss"
 
 ReactDOM.render(
   <React.StrictMode>
