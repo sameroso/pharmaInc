258205f40a29388110ce2ddaebabd01ef5abe0bb
Samer Kayali <samerkkr@gmail.com>
2021-10-17 14:36:38 -0300

    feat: finished login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import { useHistory, Link } from "react-router-dom";
 
 import { GoogleLogin } from "react-google-login";
 
@@ -16,7 +16,7 @@ export function Login() {
 
   useEffect(() => {
     if (loginObj) {
-      history.push("/dashboard")
+      history.push("/dashboard");
     }
   });
 
@@ -27,11 +27,29 @@ export function Login() {
     removeLoader();
   };
   return (
-    <GoogleLogin
-      clientId="648058795167-3ujqqb48jnmnl6a1e7t9nu7emh15gm8i.apps.googleusercontent.com"
-      buttonText="Login"
-      onSuccess={onGoogleResponse}
-      cookiePolicy={"single_host_origin"}
-    />
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "var(--color-primary)",
+      }}
+    >
+      <div className="text-center border p-5">
+        <h1 className="text-white">Pharma Inc.</h1>
+        <GoogleLogin
+          clientId="648058795167-3ujqqb48jnmnl6a1e7t9nu7emh15gm8i.apps.googleusercontent.com"
+          buttonText="Login with google"
+          onSuccess={onGoogleResponse}
+          cookiePolicy={"single_host_origin"}
+        />
+        <h4 className="text-white mt-4">Or</h4>
+        <Link to="/dashboard">
+            <h5 className="text-white">enter without login</h5>
+        </Link>
+      </div>
+    </div>
   );
 }
